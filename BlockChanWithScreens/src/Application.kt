65be6b8c047a7c3b49a1615c041d31ccba3d5cc8
Sean F Chan <sean@seanfchan.com>
2019-09-25 22:12:08 -0700

    Start of BlockChainWithScreens
    Ktor backend to serve clients so we can fine tune resposnes for the resources constrained clients.

---
+++ BlockChanWithScreens/src/Application.kt
@@ -0,0 +1,93 @@
+package com.blockchainwithscreens
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.html.*
+import kotlinx.html.*
+import kotlinx.css.*
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+import io.ktor.client.features.json.*
+import io.ktor.client.request.*
+import kotlinx.coroutines.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    val client = HttpClient(Apache) {
+        install(JsonFeature) {
+            serializer = GsonSerializer()
+        }
+    }
+    runBlocking {
+        // Sample for making a HTTP Client request
+        /*
+        val message = client.post<JsonSampleClass> {
+            url("http://127.0.0.1:8080/path/to/endpoint")
+            contentType(ContentType.Application.Json)
+            body = JsonSampleClass(hello = "world")
+        }
+        */
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+        route("/weather") {
+
+        }
+        route("/stocks") {
+
+        }
+
+
+//        get("/html-dsl") {
+//            call.respondHtml {
+//                body {
+//                    h1 { +"HTML" }
+//                    ul {
+//                        for (n in 1..10) {
+//                            li { +"$n" }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//
+//        get("/styles.css") {
+//            call.respondCss {
+//                body {
+//                    backgroundColor = Color.red
+//                }
+//                p {
+//                    fontSize = 2.em
+//                }
+//                rule("p.myclass") {
+//                    color = Color.blue
+//                }
+//            }
+//        }
+    }
+}
+
+data class JsonSampleClass(val hello: String)
+
+fun FlowOrMetaDataContent.styleCss(builder: CSSBuilder.() -> Unit) {
+    style(type = ContentType.Text.CSS.toString()) {
+        +CSSBuilder().apply(builder).toString()
+    }
+}
+
+fun CommonAttributeGroupFacade.style(builder: CSSBuilder.() -> Unit) {
+    this.style = CSSBuilder().apply(builder).toString().trim()
+}
+
+suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) {
+    this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
+}
---
+++ BlockChanWithScreens/test/ApplicationTest.kt
@@ -0,0 +1,29 @@
+package com.blockchainwithscreens
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.html.*
+import kotlinx.html.*
+import kotlinx.css.*
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+import io.ktor.client.features.json.*
+import io.ktor.client.request.*
+import kotlinx.coroutines.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() {
+        withTestApplication({ module(testing = true) }) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("HELLO WORLD!", response.content)
+            }
+        }
+    }
+}
