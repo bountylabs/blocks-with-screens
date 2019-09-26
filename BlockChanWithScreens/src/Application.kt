733209aa122f37636c087f54cbccae6a642f52bf
Sean F Chan <sean@seanfchan.com>
2019-09-26 10:36:53 -0700

    added simple cache to throttle refresh of data

--- BlockChanWithScreens/src/Application.kt
+++ BlockChanWithScreens/src/Application.kt
@@ -15,8 +15,15 @@ import io.ktor.client.features.json.*
 import io.ktor.client.request.*
 import io.ktor.features.ContentNegotiation
 import kotlinx.coroutines.*
+import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+// This simple cache is mapping from the zipcode passed in as a param to the response from the source
+val weatherZipCodeCache = HashMap<String, WeatherZipCodeResponse>()
+
+// Keeps track of the last time the data for the weather at a zip code was updated
+val weatherZipCodeLastUpdate = HashMap<String, Long>()
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
@@ -46,11 +53,21 @@ fun Application.module(testing: Boolean = false) {
         route("/weather") {
             get("/zip/{zipcode}") {
                 // Make call to open weather API
-                val zipcodeParam = call.parameters["zipcode"]
+                val gson = Gson()
+                val zipcodeParam = call.parameters["zipcode"] ?: return@get
+
+                val lastUpdate = weatherZipCodeLastUpdate[zipcodeParam]
+                if (lastUpdate != null) {
+                    if (System.currentTimeMillis() - lastUpdate < TimeUnit.MINUTES.toMillis(1)) {
+                        val response = weatherZipCodeCache[zipcodeParam]
+                        call.respondText { gson.toJson(response) }
+                        return@get
+                    }
+                }
+
                 val url = "http://api.openweathermap.org/data/2.5/weather?zip=$zipcodeParam,us&appid=aacee67b7605f7655a3dc492df9ea8a8"
                 val tempClient = HttpClient()
                 val openWeatherResponse = tempClient.get<String>(url)
-                val gson = Gson()
                 val weather = gson.fromJson(openWeatherResponse, OpenWeatherZipCodeResponse::class.java)
                 val weatherDescriptions = weather.weather[0]
                 val weatherMain = weather.main
@@ -63,11 +80,17 @@ fun Application.module(testing: Boolean = false) {
                     weatherMain.temp_min,
                     weatherMain.temp_max
                 )
+
+                weatherZipCodeLastUpdate[zipcodeParam] = System.currentTimeMillis()
+                weatherZipCodeCache[zipcodeParam] = response
                 call.respondText { gson.toJson(response) }
             }
         }
         route("/stocks") {
-
+            get("/symbol/{symbol}") {
+                // Make call to alpha vantage, TIME_SERIES_DAILY
+                // Parse "Meta Data" and "Times Series (Daily)" for the lastest day, the close price always represents the most up to date price
+            }
         }
 
 
