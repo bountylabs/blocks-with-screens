d2fe0e501292ea4f3a765f9e17bfd6baaa76947a
magus <noah@iamnoah.com>
2020-11-29 23:47:40 -0800

    www: canvas texture for block screen

--- www/components/WebGLDemo.js
+++ www/components/WebGLDemo.js
@@ -17,6 +17,8 @@ import {
 import WebGLStats from "./WebGLStats";
 import WebGLOrbitControls from "./WebGLOrbitControls";
 
+const MODEL_SIZE = 57.55298173283222;
+
 export default function WebGLDemo() {
   return (
     <Canvas
@@ -33,7 +35,10 @@ export default function WebGLDemo() {
       <spotLight intensity={5} position={[0, 50, 0]} penumbra={1} castShadow />
       <fog attach="fog" args={["white", 100, 250]} />
 
-      <BlockWithScreen />
+      <GentleFloatEffect>
+        <BlockWithScreen />
+        <VirtualBlox />
+      </GentleFloatEffect>
 
       <mesh
         position={[0, -25, 0]}
@@ -53,14 +58,12 @@ export default function WebGLDemo() {
 
       {/* debug */}
       {/* <WebGLStats /> */}
-      {/* <gridHelper args={[100, 100]} rotation={[Math.PI / 2, 0, 0]} /> */}
+      <gridHelper args={[100, 100]} rotation={[Math.PI / 2, 0, 0]} />
     </Canvas>
   );
 }
 
-function BlockWithScreen(props) {
-  const [active, set_active] = React.useState(false);
-
+function GentleFloatEffect({ children, ...props }) {
   // subtle floaty effect
   // create a common spring that will be used later to interpolate other values
   const [direction, set_direction] = React.useState(false);
@@ -81,6 +84,19 @@ function BlockWithScreen(props) {
     }
   });
 
+  return (
+    <a.mesh scale={[1, 1, 1]} position={pos} {...props}>
+      {children}
+    </a.mesh>
+  );
+}
+
+function BlockWithScreen(props) {
+  const [active, set_active] = React.useState(false);
+
+  // This reference will give us direct access to the mesh
+  const mesh = React.useRef();
+
   React.useEffect(() => {
     traverseMaterials(mesh.current, (material) => {
       material.wireframe = active;
@@ -90,6 +106,8 @@ function BlockWithScreen(props) {
   function handleLoadedModel(model) {
     const box = new THREE.Box3().setFromObject(model);
     const size = box.getSize(new THREE.Vector3()).length();
+
+    console.debug(size);
 
     model.position.x = (-1 * size) / 4;
     model.position.y = (-1 * size) / 4;
@@ -101,27 +119,26 @@ function BlockWithScreen(props) {
   const boxSize = 40;
 
   return (
-    <a.mesh
-      {...props}
+    <mesh
       scale={[1, 1, 1]}
-      position={pos}
       onPointerUp={(event) => {
         // setTimeout required to prevent racing
         // without setTimeout the wireframes may flicker and reset
         setTimeout(() => set_active(!active), 0);
       }}
+      {...props}
     >
       <mesh>
         <boxBufferGeometry args={[boxSize, boxSize, boxSize]} />
         <meshStandardMaterial transparent opacity={0} color={"red"} />
       </mesh>
 
-      <a.mesh ref={mesh} scale={[1, 1, 1]}>
+      <mesh ref={mesh} scale={[1, 1, 1]}>
         <React.Suspense fallback={<Box scale={[25, 25, 25]} />}>
           <BlockWithScreenFBX onLoad={handleLoadedModel} />
         </React.Suspense>
-      </a.mesh>
-    </a.mesh>
+      </mesh>
+    </mesh>
   );
 }
 
@@ -174,4 +191,52 @@ function traverseMaterials(object, callback) {
       : [node.material];
     materials.forEach(callback);
   });
+}
+
+function VirtualBlox(props) {
+  //create canvas for image texture
+  const canvas = document.createElement("canvas");
+  const g = canvas.getContext("2d");
+  const canvasSize = 1024;
+  canvas.width = canvas.height = canvasSize;
+  g.fillStyle = "rgba(255,0,0,1)";
+  g.fillRect(0, 0, canvas.width, canvas.height);
+  g.fillStyle = "black";
+  g.strokeRect(0, 0, canvas.width, canvas.height);
+
+  // draw text on canvas
+  const text = "VirtualBlox";
+  g.font = "Bold 72px Arial";
+  g.fillStyle = "white";
+  g.strokeStyle = "black";
+  const {
+    actualBoundingBoxAscent: textHeight,
+    width: textWidth,
+    ...textMetrics
+  } = g.measureText(text);
+  const x = canvasSize / 2 - textWidth / 2;
+  const y = canvasSize / 2 - textHeight / 2;
+  g.fillText(text, x, y);
+  g.strokeText(text, x, y);
+
+  const boxMult = 0.5 + 1 / Math.pow(2, 3 + 1 / Math.pow(2, 1));
+  const boxSize = MODEL_SIZE * boxMult;
+
+  // canvas contents will be used for a texture
+  const texture = new THREE.Texture(canvas);
+  texture.needsUpdate = true;
+
+  // console.debug({ boxMult, boxSize });
+
+  return (
+    <mesh
+      {...props}
+      scale={[1, 1, 1]}
+      position={[-1.25, -1.25, -1 * (MODEL_SIZE / 4 + 1.8)]}
+      rotation={[0, Math.PI, 0]}
+    >
+      <planeBufferGeometry args={[boxSize, boxSize]} attach="geometry" />
+      <meshStandardMaterial attach="material" map={texture} />
+    </mesh>
+  );
 }
