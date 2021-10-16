7306ee1861f6b4a133ef7c99494dbb83a2563cd1
John Boiles <johnaboiles@gmail.com>
2021-10-15 23:46:55 -0700

    Fix the fireworks blockware

--- blockware/fireworks/lib/Particle/Particle.h
+++ blockware/fireworks/lib/Particle/Particle.h
@@ -10,14 +10,14 @@ const int MIN_X = floor(0.1 * 128);
 const int MIN_HEIGHT = floor(0.8 * 128);
 const int MAX_X = floor(0.8 * 128);
 const float EXPLODE_CHANCE = 0.08;
-const int EXPLOSION_PARTICLES = 16;
+const int EXPLOSION_PARTICLES = 22;
 const int MAX_AGE = 5;
 
 const std::vector<unsigned int> FIREWORK_COLORS{ BLUE,RED,GREEN,CYAN,MAGENTA,YELLOW,DARK_GRAY,GRAY,LIGHT_GRAY };
 
 static inline int randomFireworkColor()
 {
-  return FIREWORK_COLORS[floor(FIREWORK_COLORS.size() * random())];
+  return FIREWORK_COLORS[floor(FIREWORK_COLORS.size() * randomf())];
 }
 
 
